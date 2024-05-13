43c9ba98cf4badf5fa7444647ac09afabad4c48f
Rupal Hiwarkar <167731559+rupalxxiii@users.noreply.github.com>
2024-05-13 07:26:15 -0600

    Add files via upload

---
+++ US Retail SuperStore Sales Analysis.sql
@@ -0,0 +1,96 @@
+Create database us_retail_superstore;
+
+use us_retail_superstore;
+
+-- drop table samplesuperstore if exist;
+
+select * from samplesuperstore;
+
+select count(*) from samplesuperstore;
+
+-- Scenario 1 -- Summary Statistics: 
+-- Obtain summary statistics such as total sales, average sales, total profit, average profit, etc., 
+-- to understand the overall performance.
+
+SELECT 
+    SUM(Sales) AS TotalSales,
+    AVG(Sales) AS AverageSales,
+    SUM(Profit) AS TotalProfit,
+    AVG(Profit) AS AverageProfit
+FROM  samplesuperstore;
+
+
+-- Scenario 2-- Sales Distribution by Category: 
+-- Analyze sales distribution across different categories to identify top-selling categories.
+
+SELECT 
+    Category,
+    SUM(Sales) AS TotalSales
+FROM 
+    samplesuperstore
+GROUP BY 
+    Category
+ORDER BY 
+    TotalSales DESC;
+    
+    
+-- Scenario 3 -- Top Selling Products: 
+-- Identify the top-selling products based on sales quantity or revenue.
+
+desc samplesuperstore;
+
+-- Scenario 4 -- ALTER TABLE samplesuperstore
+-- RENAME COLUMN Sub-Categry TO sub_Category;                 giving error
+
+ALTER TABLE samplesuperstore
+CHANGE COLUMN `Sub-Category` sub_Category varchar(50);
+ 
+SELECT 
+    Sub_Category,
+    SUM(Quantity) AS TotalQuantity,
+    SUM(Sales) AS TotalSales
+FROM 
+    samplesuperstore
+GROUP BY 
+    Sub_Category
+ORDER BY 
+    TotalSales DESC;
+    
+    
+ -- Scenario 5 -- Profit Analysis by Segment: 
+ -- Analyze profit across different market segments. 
+ 
+ SELECT 
+    Segment,
+    SUM(Profit) AS TotalProfit
+FROM 
+    samplesuperstore
+GROUP BY 
+    Segment;
+
+
+-- Scenario 6 -- Discount Impact on Profit: 
+-- Investigate the impact of discounts on profit.
+
+SELECT 
+    Discount,
+    AVG(Profit) AS AverageProfit
+FROM 
+    samplesuperstore
+GROUP BY 
+    Discount
+ORDER BY 
+    Discount;
+
+-- Scenario 7-- Geographical Analysis: 
+-- Analyze sales performance by country, city, or region.
+
+SELECT 
+    Country,
+    SUM(Sales) AS TotalSales
+FROM 
+    samplesuperstore
+GROUP BY 
+    Country
+ORDER BY 
+    TotalSales DESC;
